fa29392341962603897eeb0ee25b252c11a12abc
Harshit <harshit537cyber@gmail.com>
2026-04-20 15:13:53 +0530

    ok

--- src/home-page/Home.jsx
+++ src/home-page/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../component/Header'
 import Footer from '../component/Footer'
+import Hero from './hero/Hero'
 
 const Home = () => {
   return (
     <>
       <Header/>
+      <Hero/>
       <Footer/>
     </>
   )
